28786c7bd565776a942de167264c02b5bebf0fbb
Ricardo Valfreixo <ricardo.coding@gmail.com>
2024-05-14 11:10:21 +0200

    added some laravel stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -155,8 +155,8 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  -- -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {}, lazy = false },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
